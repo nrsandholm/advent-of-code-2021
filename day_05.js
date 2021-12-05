5cfd2720b8fdbf14603941e77e970e82d5295a62
Niko <me@home>
2021-12-06 01:13:56 +0200

    Refactor day 5

--- day_05.js
+++ day_05.js
@@ -10,10 +10,6 @@ const { coordinates, maxX, maxY } = input
     const [toX, toY] = to.split(',').map(v => parseInt(v));
     result.maxX = Math.max(result.maxX, fromX, toX);
     result.maxY = Math.max(result.maxY, fromY, toY);
-    /* TODO Ought to rewrite this. The full set of coordinates should be generated here.
-      Then it would be only a matter of incrementing the hits in the matrix. Would be no
-      need to guess if dialognal or not...
-    */
     result.coordinates.push({ fromX, fromY, toX, toY });
     return result;
   }, {
@@ -38,55 +34,37 @@ const initMatrix = (maxX, maxY) => {
   return Array(maxY + 1).fill(null).map(() => Array(maxX + 1).fill(0));
 };
 
-const isHorizontalOrVertical = (fromX, toX, fromY, toY) => {
-  return fromX === toX || fromY === toY;
+const getCoordinate = (index, from, to) => {
+  if (from > to) {
+    return from - index;
+  }
+  else if (from < to) {
+    return from + index;
+  }
+  else {
+    return from;
+  }
 }
 
-const isBetween = (v, fromV, toV) => {
-  if (fromV > toV) return (v <= fromV && v >= toV);
-  if (fromV < toV) return (v >= fromV && v <= toV);
-  return v === fromV || v === toV;
-};
-
-const isBetweenDiagonal = (x, y, fromX, toX, fromY, toY) => {
-  if (x === fromX && y === fromY) return true;
-  if (x === toX && y === toY) return true;
-  if (!isBetween(x, fromX, toX)) return false;
-  if (!isBetween(y, fromY, toY)) return false;
-  // Case 1,5 to 5,1
-  if (fromX < toX && fromY > toY) {
-    return (x - fromX) === (fromY - y) && (toX - x) === (y - toY);
-  }
-  // Case 1,1 to 5,5
-  if (fromX < toX && fromY < toY) {
-    return (x - fromX) === (y - fromY) && (toX - x) === (toY - y);
-  }
-  // Case 5,5 to 1,1
-  if (fromX > toX && fromY > toY) {
-    return (fromX - x) === (fromY - y) && (x - toX) === (y - toY);
-  }
-  // Case 5,1 to 1,5
-  if (fromX > toX && fromY < toY) {
-    return (x - fromX) === (fromY - y) && (toX - x) === (y - toY);
-  }
-  throw new Error('WTF');
+const generateLine = (fromX, toX, fromY, toY) => {
+  const length = Math.max(1 + Math.abs(fromX - toX), 1 + Math.abs(fromY - toY));
+  return Array(length)
+    .fill(null)
+    .map((_, i) => {
+      return [getCoordinate(i, fromX, toX), getCoordinate(i, fromY, toY)]
+    });
 };
 
 // Part 1
 (function () {
   const assignCoordinates = (matrix, coordinates) => {
     for (let {fromX, toX, fromY, toY} of coordinates) {
-      if (! isHorizontalOrVertical(fromX, toX, fromY, toY)) {
+      if (! (fromX === toX || fromY === toY)) {
         continue;
       }
-      for (let y = 0; y < matrix.length; y++) {
-        for (let x = 0; x < matrix[y].length; x++) {
-          const yInBetween = isBetween(y, fromY, toY);
-          const xInBetween = isBetween(x, fromX, toX);
-          if (xInBetween && yInBetween) {
-            matrix[y][x] += 1;
-          }
-        }
+      const line = generateLine(fromX, toX, fromY, toY);
+      for (let [x, y] of line) {
+        matrix[y][x] += 1;
       }
     }
   };
@@ -102,21 +80,9 @@ const isBetweenDiagonal = (x, y, fromX, toX, fromY, toY) => {
 (function () {
   const assignCoordinates = (matrix, coordinates) => {
     for (let {fromX, toX, fromY, toY} of coordinates) {
-      for (let y = 0; y < matrix.length; y++) {
-        for (let x = 0; x < matrix[y].length; x++) {
-          // Horizontal or vertical
-          if (isHorizontalOrVertical(fromX, toX, fromY, toY)) {
-            const yInBetween = isBetween(y, fromY, toY);
-            const xInBetween = isBetween(x, fromX, toX);
-            if (xInBetween && yInBetween) {
-              matrix[y][x] += 1;
-            }
-          }
-          // Diagonal
-          else if (isBetweenDiagonal(x, y, fromX, toX, fromY, toY)) {
-            matrix[y][x] += 1;
-          }
-        }
+      const line = generateLine(fromX, toX, fromY, toY);
+      for (let [x, y] of line) {
+        matrix[y][x] += 1;
       }
     }
   };
@@ -126,47 +92,4 @@ const isBetweenDiagonal = (x, y, fromX, toX, fromY, toY) => {
   // drawDiagram(matrix);
   const count = countOverlaps(matrix);
   console.log(count);
-})();
-
-// Tests
-(function () {
-  let fromX = 1, toX = 5, fromY = 1, toY = 5;
-  if (isBetweenDiagonal(0, 0, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(6, 6, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(1, 0, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(1, 2, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(2, 1, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(2, 3, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(3, 2, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(3, 4, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(4, 3, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(4, 5, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(5, 4, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(5, 6, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(1, 1, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(2, 2, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(3, 3, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(4, 4, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(5, 5, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-
-  fromX = 5; toX = 1; fromY = 1; toY = 5;
-  if (isBetweenDiagonal(0, 0, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(6, 6, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(5, 2, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(5, 0, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(4, 1, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(4, 3, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(3, 4, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(3, 2, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(2, 3, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(2, 5, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(1, 6, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (isBetweenDiagonal(1, 4, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(5, 1, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(4, 2, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(3, 3, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(2, 4, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-  if (!isBetweenDiagonal(1, 5, fromX, toX, fromY, toY)) throw new Error('Wrong!');
-
-  console.log('All passed!');
 })();
